d34199bdc1e8318ac9d7b62a3c2d44b7f0343407
Hicham <h.kassou91@gmail.com>
2020-04-17 12:35:37 +0200

    modification du fichier requete.php

--- requete.php
+++ requete.php
@@ -1,10 +1,12 @@
-<?php 
+<?php
+require 'bdd/connexionBdd.php'; 
 if(isset($_POST['number'], $_POST['photos'], $_POST['type'], $_POST['version'])){
-    if(!empty($_POST['number']) AND !empty($_POST['photos']) AND !empty($_POST['type']) AND !empty($_POST['version'])){
+    if(!empty($_POST['number']) AND !empty($_POST['photos']) AND !empty($_POST['type']) AND !empty($_POST['version']) AND !empty($_POST['nom'])){
         $numero=htmlspecialchars($_POST['number']);
         $photos=htmlspecialchars($_POST['photos']);
         $type=htmlspecialchars($_POST['type']);
         $version=htmlspecialchars($_POST['version']);
+        $nom=htmlspecialchars($_POST['nom']);
 
         $req=$mysqli->query("INSERT INTO Pokemon(NumeroPokemon,Image,Type,Version) VALUES('$numero','$photos','$type','$version')");
         $message='Le formulaire a bien été envoyé';
@@ -19,6 +21,7 @@ if(isset($_POST['number'], $_POST['photos'], $_POST['type'], $_POST['version']))
 <div align=center>
     <form action="" method="post" enctype="multipart/form-data">
         <p><label for="numero">Numéro du pokemon : <input type="text" name="number"></label></p>
+        <p><label for="nom">Nom du pokemon : <input type="text" name="nom"></label></p>
         <p><label for="image"> Image :<input type="text" name="photos"></label></p>
         <p><label for="type">Type :<input type="text" name="type"></label></p>
         <p><label for="version">Version : <input type="select" name="version"></label></p>
