7a750247983d6c457709d3a8304e6b9987b42f3b
Cedric Ktourza <cedric.ktourza@gmail.com>
2020-04-17 12:27:25 +0200

    Ajout du champs nom ds le form

--- requete.php
+++ requete.php
@@ -1,13 +1,18 @@
-<?php 
-if(isset($_POST['number'], $_POST['photos'], $_POST['type'], $_POST['version'])){
-    if(!empty($_POST['number']) AND !empty($_POST['photos']) AND !empty($_POST['type']) AND !empty($_POST['version'])){
+<?php
+require 'bdd/connexionBdd.php';
+?>
+
+<?php
+if(isset($_POST['nom'], $_POST['number'], $_POST['photos'], $_POST['type'], $_POST['version'])){
+    if(!empty($_POST['nom']) AND!empty($_POST['nom']) AND !empty($_POST['photos']) AND !empty($_POST['type']) AND !empty($_POST['version'])){
+        $nom=htmlspecialchars($_POST['nom']);
         $numero=htmlspecialchars($_POST['number']);
         $photos=htmlspecialchars($_POST['photos']);
         $type=htmlspecialchars($_POST['type']);
         $version=htmlspecialchars($_POST['version']);
 
-        $req=$bdd->prepare('INSERT INTO pokemon(numero,image,type,version) VALUES(?,?,?,?)');
-        $req->execute(array($numero,$photos,$type,$version));
+        $req=$bdd->prepare('INSERT INTO pokemon(nom,NumeroPokemon,Image,Type,Version) VALUES(?,?,?,?,?)');
+        $req->execute(array($nom,$numero,$photos,$type,$version));
         $message='Le formulaire a bien été envoyé';
 
     }else{
@@ -18,7 +23,8 @@ if(isset($_POST['number'], $_POST['photos'], $_POST['type'], $_POST['version']))
 ?>
 
 <div align=center>
-    <form action="" method="post" enctype="multipart/form-data">
+    <form action="" method="post">
+        <p><label for="nom">Nom du pokémon : <input type="text" name="nom"></label></p>
         <p><label for="numero">Numéro du pokemon : <input type="text" name="number"></label></p>
         <p><label for="image"> Image :<input type="file" name="photos"></label></p>
         <p><label for="type">Type :<input type="text" name="type"></label></p>
