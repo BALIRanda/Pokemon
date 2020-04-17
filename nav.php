6d2700cb5ca964c2ae361ff5ac4eb754f396aad8
Cedric Ktourza <cedric.ktourza@gmail.com>
2020-04-17 14:49:50 +0200

    Fichier Nav comprenant le CSS + BOOTSRAP

---
+++ nav.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Tout les pokemons</title>
+  </head>
+  <meta charset="utf-8">
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link href="style.css" rel="stylesheet" type="text/css">
+
+  <body>
+
+<div id="banniere">
+    <img src="images/pokemon-banner.png" alt="pokemonbani" />
+</div>
