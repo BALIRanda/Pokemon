bc5a7b2e168953dc15cfd05d4e863c834be53daa
Cedric Ktourza <cedric.ktourza@gmail.com>
2020-04-17 11:32:21 +0200

    Creation formulaire plus requete sql

---
+++ requete.php
@@ -0,0 +1,31 @@
+<?php 
+if(isset($_POST['number'], $_POST['photos'], $_POST['type'], $_POST['version'])){
+    if(!empty($_POST['number']) AND !empty($_POST['photos']) AND !empty($_POST['type']) AND !empty($_POST['version'])){
+        $numero=htmlspecialchars($_POST['number']);
+        $photos=htmlspecialchars($_POST['photos']);
+        $type=htmlspecialchars($_POST['type']);
+        $version=htmlspecialchars($_POST['version']);
+
+        $req=$bdd->prepare('INSERT INTO pokemon(numero,image,type,version) VALUES(?,?,?,?)');
+        $req->execute(array($numero,$photos,$type,$version));
+        $message='Le formulaire a bien été envoyé';
+
+    }else{
+        $message='Veuillez remplir tous les champs';
+    }
+}
+
+?>
+
+<div align=center>
+    <form action="" method="post" enctype="multipart/form-data">
+        <p><label for="numero">Numéro du pokemon : <input type="text" name="number"></label></p>
+        <p><label for="image"> Image :<input type="file" name="photos"></label></p>
+        <p><label for="type">Type :<input type="text" name="type"></label></p>
+        <p><label for="version">Version : <input type="select" name="version"></label></p>
+        <input type="submit" name="envoyer">
+    </form>
+    <?php if(isset($message)){
+        echo $message;
+    } ?>
+</div>
