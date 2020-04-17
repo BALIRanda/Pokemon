8ad7544d9a0851a3df1e3e15865d10dc6638e946
Hicham <h.kassou91@gmail.com>
2020-04-17 12:13:02 +0200

    modification du fichier requete.php

--- requete.php
+++ requete.php
@@ -6,8 +6,7 @@ if(isset($_POST['number'], $_POST['photos'], $_POST['type'], $_POST['version']))
         $type=htmlspecialchars($_POST['type']);
         $version=htmlspecialchars($_POST['version']);
 
-        $req=$bdd->prepare('INSERT INTO pokemon(numero,image,type,version) VALUES(?,?,?,?)');
-        $req->execute(array($numero,$photos,$type,$version));
+        $req=$mysqli->query("INSERT INTO Pokemon(NumeroPokemon,Image,Type,Version) VALUES('$numero','$photos','$type','$version')");
         $message='Le formulaire a bien été envoyé';
 
     }else{
@@ -20,7 +19,7 @@ if(isset($_POST['number'], $_POST['photos'], $_POST['type'], $_POST['version']))
 <div align=center>
     <form action="" method="post" enctype="multipart/form-data">
         <p><label for="numero">Numéro du pokemon : <input type="text" name="number"></label></p>
-        <p><label for="image"> Image :<input type="file" name="photos"></label></p>
+        <p><label for="image"> Image :<input type="text" name="photos"></label></p>
         <p><label for="type">Type :<input type="text" name="type"></label></p>
         <p><label for="version">Version : <input type="select" name="version"></label></p>
         <input type="submit" name="envoyer">
